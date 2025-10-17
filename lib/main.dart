d174d754223cdab2ea6607c904b9cbdcf5e93668
AbdulteefRashid <abdulte97@gmail.com>
2025-10-18 00:40:39 +0300

    create repo

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'features/pets/presentation/screens/splash_screen.dart';
+
+void main() {
+  runApp(const PetFinderApp());
+}
+
+class PetFinderApp extends StatelessWidget {
+  const PetFinderApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'PetFinder App',
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      home: const SplashScreen(),
+    );
+  }
+}
